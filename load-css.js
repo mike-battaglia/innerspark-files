d664c8c42572897ebb5d7329656e12ebe747fddb
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2022-06-01 09:44:16 -0500

    Create load-css.js

---
+++ load-css.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+
+var link = document.createElement( "link" );
+link.href = "https://mike-battaglia.github.io/innerspark-files/style.css";
+link.type = "text/css";
+link.rel = "stylesheet";
+link.media = "screen,print";
+
+document.getElementsByTagName( "head" )[0].appendChild( link );
+</script>
