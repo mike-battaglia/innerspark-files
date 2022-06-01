43ffcd1e04b6487488842011cbe5ac6a4815d63f
Mike Battaglia <70967369+mike-battaglia@users.noreply.github.com>
2022-06-01 09:15:36 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
 console.log("Hey, Gitling! This code is coming from a GitHub Repo. 0830");
-
-//comment-add confirmation
-let commentBtn = document.querySelector("[kjb-settings-id=language_comment_add]");
-commentBtn.onclick=function(){
-    console.log("clicked");
-};
